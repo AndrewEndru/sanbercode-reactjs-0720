2eca5f7659fea4691201e7e3a68bca16ab70b37b
Andrew Endru <Andrewtaghulihi21@gmail.com>
2020-08-02 10:43:34 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { render } from '@testing-library/react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      <div id="container">
+        <h2>Form Penjualan Buah</h2>
+        <table>
+          <tr>
+            <th>Nama</th>
+            <td><input type="text" style={{ paddingRight: "60px" }}></input></td>
+          </tr>
+          <tr>
+            <th rowSpan="5" style={{ paddingTop: "110px" }}>Daftar Item</th>
+            <td>
+              <input type="checkbox" id="semangka"></input>
+              <label>Semangka</label>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <input type="checkbox"></input>
+              <label>Jeruk</label>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <input type="checkbox"></input>
+              <label>Nanas</label>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <input type="checkbox"></input>
+              <label>Salak</label>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <input type="checkbox"></input>
+              <label>Anggur</label>
+            </td>
+          </tr>
+          <tr>
+            <th style={{ paddingTop: "10px" }}>
+              <button>Kirim</button>
+            </th>
+          </tr>
+        </table>
+      </div >
     </div>
   );
 }
